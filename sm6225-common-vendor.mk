89ca992fb423ae68a2a5fa2fb8fa6ccb558d35bb
boedhack99 <boedhack@gmail.com>
2022-08-13 21:05:27 +0700

    sm6225-common: Decommonize power-off charging * We have diff resolution device * spes = 2400 x 1080 * fog = 1650 x 720
    Change-Id: I8c38b2d9d8ee2da262bcc3127082f54ca22ca12c

--- sm6225-common-vendor.mk
+++ sm6225-common-vendor.mk
@@ -6,7 +6,6 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/sm6225-common
 
 PRODUCT_COPY_FILES += \
-    vendor/xiaomi/sm6225-common/proprietary/bin/chargeonlymode:$(TARGET_COPY_OUT_SYSTEM)/bin/chargeonlymode \
     vendor/xiaomi/sm6225-common/proprietary/etc/permissions/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/privapp-permissions-qti.xml \
     vendor/xiaomi/sm6225-common/proprietary/etc/sysconfig/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
     vendor/xiaomi/sm6225-common/proprietary/framework/tcmclient.jar:$(TARGET_COPY_OUT_SYSTEM)/framework/tcmclient.jar \
